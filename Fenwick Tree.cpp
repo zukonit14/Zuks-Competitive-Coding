68547a88195ad5e76b0251e58c0b493cc3d050af
Kunal Raut <47981796+zukonit14@users.noreply.github.com>
2020-07-22 10:34:05 +0530

    Create Fenwick Tree.cpp

---
+++ Fenwick Tree.cpp
@@ -0,0 +1,15 @@
+ll bit[MAXN];
+
+void upd(ll i,ll x) 
+{
+    for(++i;i<n;i+=i&-i)
+        bit[i]+=x;
+}
+
+ll qry(ll i)    
+{
+    ll r=0;
+    for(;i;i-=i&-i)
+        r+=bit[i];
+    return r;
+}
