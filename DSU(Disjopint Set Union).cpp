8542b8684e8da2a13ba5684419ceb73e4a312e97
Kunal Raut <47981796+zukonit14@users.noreply.github.com>
2020-08-03 23:39:28 +0530

    Update DSU(Disjopint Set Union).cpp

--- DSU(Disjopint Set Union).cpp
+++ DSU(Disjopint Set Union).cpp
@@ -1,45 +1,49 @@
 struct DSU
 {
-	ll connected;
-	vector<ll> par, sz;
+    ll connected;
+    vector<ll> par, sz;
 
-	void init(ll n) 
-	{
-		par = sz = vector<ll> (n + 1, 0);
-		for(ll i = 1; i <= n; i++)
-			par[i] = i, sz[i] = 1;
-		connected = n;
-	}
+    void init(ll n)
+    {
+        par = sz = vector<ll> (n + 1, 0);
+        for (ll i = 1; i <= n; i++)
+            par[i] = i, sz[i] = 1;
+        connected = n;
+    }
 
-	ll getPar(ll u)
-	{
-		while(u != par[u])
-		{
-			par[u] = par[par[u]];
-			u = par[u];
-		}
-		return u;
-	}
+    ll getpar(ll u)
+    {
+        while (u != par[u])
+        {
+            par[u] = par[par[u]];
+            u = par[u];
+        }
+        return u;
+    }
 
-	ll getSize(ll u)
-	{
-		return sz[getPar(u)];
-	}
+    ll getsize(ll u)
+    {
+        return sz[getpar(u)];
+    }
 
-	void unite(ll u, ll v)
-	{
-		ll par1 = getPar(u), par2 = getPar(v);
+    void unite(ll u, ll v)
+    {
+        ll par1 = getpar(u), par2 = getpar(v);
 
-		if(par1 == par2)
-			return;
+        if (par1 == par2)
+            return;
 
-		connected--;
+        connected--;
 
-		if(sz[par1] > sz[par2])
-			swap(par1, par2);
+        if (sz[par1] > sz[par2])
+            swap(par1, par2);
 
-		sz[par2] += sz[par1];
-		sz[par1] = 0;
-		par[par1] = par[par2];
-	}
+        sz[par2] += sz[par1];
+        sz[par1] = 0;
+        par[par1] = par[par2];
+    }
 };
+
+
+//DSU dsu;
+//dsu.init(n);
