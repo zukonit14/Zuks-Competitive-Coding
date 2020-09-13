d2bb94fc44b60e93498786f211aeee8859be2e21
Kunal Raut <47981796+zukonit14@users.noreply.github.com>
2020-09-13 23:38:50 +0530

    Update Toposort.cpp

--- Toposort.cpp
+++ Toposort.cpp
@@ -1,4 +1,36 @@
 vector<ll>order;
+vector<int> order; //Stores the Topological Order
+
+bool toposort(ll n) //Returns 1 if there exists a toposort, 0 if there is a cycle
+{	
+	queue<ll> q;
+	vector<ll> indeg(n + 1, 0);
+	for(ll i = 1; i <= n; i++)
+		for(auto &it:g[i])
+			indeg[it]++;
+	for(ll i = 1; i <= n; i++)
+	{
+		if(!indeg[i])
+			q.push(i);
+	}
+	while(!q.empty())
+	{
+		ll u = q.front();
+		q.pop();
+		order.push_back(u);
+		for(auto &v:g[u])
+		{
+			indeg[v]--;
+			if(!indeg[v])
+				q.push(v);
+		}
+	}
+	return (order.size() == n);
+}
+
+
+
+//Also another implementation for finding order if toposort exists;
 void dfs(ll u)
 {
     vis[u] = 1;
